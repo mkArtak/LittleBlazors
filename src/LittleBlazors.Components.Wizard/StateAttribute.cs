5da7683722177c79bc8aac9576c8f77845dcb84b
mkArtak <mkArtak@users.noreply.github.com>
2020-04-22 02:03:31 -0700

    Added state-management capabilities for page properties. Moved the render detection logic to be based of the `ShouldRender` method.

---
+++ src/LittleBlazors.Components.Wizard/StateAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LittleBlazors.Components.Wizards
+{
+    [System.AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public sealed class StateAttribute : Attribute
+    {
+        public string Key { get; }
+
+        // This is a positional argument
+        public StateAttribute(string key)
+        {
+            this.Key = key;
+        }
+    }
+}
---
+++ src/LittleBlazors.Components.Wizard/WizardStateManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LittleBlazors.Components.Wizards
+{
+    public class WizardStateManager
+    {
+        private IDictionary<string, object> state = new Dictionary<string, object>();
+
+        public bool TryGet(string key, out object result)
+        {
+            return this.state.TryGetValue(key, out result);
+        }
+
+        public void Set(string key, object value)
+        {
+            this.state[key] = value;
+        }
+    }
+}
