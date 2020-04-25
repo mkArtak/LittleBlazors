f071c3341ca57897d1ac1a6e3c37b7de3c3bf901
mkArtak <mkArtak@users.noreply.github.com>
2020-04-25 11:33:41 -0700

    - Added concept of a `ButtonStyle` and changed replaced all button style properties with ButtonStyle properties. - Added support for controlling visibility of individual buttons

---
+++ src/LittleBlazors.Components.Core/ButtonStyle.cs
@@ -0,0 +1,47 @@
+﻿namespace LittleBlazors.Components.Core
+{
+    /// <summary>
+    /// Specifies styles applicable to a button.
+    /// </summary>
+    public readonly struct ButtonStyle
+    {
+        /// <summary>
+        /// Specifies the text of the button.
+        /// </summary>
+        public string Title { get; }
+
+        /// <summary>
+        /// Specifies the CSS class name to apply to the button, when rendered.
+        /// </summary>
+        public string CssClassName { get; }
+
+        /// <summary>
+        /// Specifies, whether the button this style applies to should be rendered or not.
+        /// </summary>
+        /// <remarks>The default value is `true`.</remarks>
+        public bool Visible { get; }
+
+        /// <summary>
+        /// Initializes a new instance of a ButtonStyle with default visibility value of `true`
+        /// </summary>
+        /// <param name="title">The title of the button</param>
+        /// <param name="cssClassName">The CSS class name to be applied to the button which this style is applied to</param>
+        public ButtonStyle(string title, string cssClassName) : this(title, cssClassName, true)
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of a ButtonStyle
+        /// </summary>
+        /// <param name="title">The title of the button</param>
+        /// <param name="cssClassname">The CSS class name to be applied to the button which this style is applied to</param>
+        /// <param name="visible">Indicates whether the button should be visible or not</param>
+        public ButtonStyle(string title, string cssClassname, bool visible)
+        {
+            this.Title = title;
+            this.CssClassName = cssClassname;
+            this.Visible = visible;
+        }
+    }
+}
---
+++ src/LittleBlazors.Components.Core/Extensions/ButtonStyleExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace LittleBlazors.Components.Core.Extensions
+{
+    public static class ButtonStyleExtensions
+    {
+        /// <summary>
+        /// Creates a new ButtonStyle instance with the specified title, while keeping the rest of the properties the same as the current style
+        /// </summary>
+        /// <param name="style">The style to use as a base</param>
+        /// <param name="title">The new title</param>
+        /// <returns>A new ButtonStyle instance with requested values</returns>
+        public static ButtonStyle Title(this ButtonStyle style, string title)
+        {
+            return new ButtonStyle(title, style.CssClassName, style.Visible);
+        }
+
+        /// <summary>
+        /// Creates a new ButtonStyle instance with the specified CSS class name value, while keeping the rest of the properties the same as the current style
+        /// </summary>
+        /// <param name="style">The style to use as a base</param>
+        /// <param name="cssClass">The new CSS class name</param>
+        /// <returns>A new ButtonStyle instance with requested values</returns>
+        public static ButtonStyle CssClassName(this ButtonStyle style, string cssClass)
+        {
+            return new ButtonStyle(style.Title, cssClass, style.Visible);
+        }
+
+        /// <summary>
+        /// Creates a new ButtonStyle instance with the specified visibility value, while keeping the rest of the properties the same as the current style
+        /// </summary>
+        /// <param name="style">The style to use as a base</param>
+        /// <param name="visible">The new visibility value</param>
+        /// <returns>A new ButtonStyle instance with requested values</returns>
+        public static ButtonStyle Visible(this ButtonStyle style, bool visible)
+        {
+            return new ButtonStyle(style.Title, style.CssClassName, visible);
+        }
+    }
+}
