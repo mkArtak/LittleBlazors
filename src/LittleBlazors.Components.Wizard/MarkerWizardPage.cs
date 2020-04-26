b6e62a65f7a3a0ad256be4c2e5454c9bd1e85b2e
mkArtak <mkArtak@users.noreply.github.com>
2020-04-25 17:00:10 -0700

    Fixed the issue causing the loading of the first wizard page not to fire

--- src/LittleBlazors.Components.Wizard/MarkerWizardPage.cs
+++ src/LittleBlazors.Components.Wizard/MarkerWizardPage.cs
@@ -2,7 +2,9 @@
 
 namespace LittleBlazors.Components.Wizards
 {
-    internal class MarkerWizardPage : WizardPage
+    /// TODO: This component should be internal.
+    /// Due to https://github.com/dotnet/aspnetcore/issues/21213 bug, this is temporarily marked as `public`.
+    public class MarkerWizardPage : WizardPage
     {
         protected override async Task OnInitializedAsync()
         {
