13565fc6c5394f8c58b081598732d86b2c3132bf
mkArtak <mkArtak@users.noreply.github.com>
2020-05-07 23:36:39 -0700

    Added readonly-state capabilities.

--- src/LittleBlazors.Components.Wizard/StateAttribute.cs
+++ src/LittleBlazors.Components.Wizard/StateAttribute.cs
@@ -2,10 +2,18 @@
 
 namespace LittleBlazors.Components.Wizards
 {
-    [System.AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
     public sealed class StateAttribute : Attribute
     {
+        /// <summary>
+        /// Gets the key of the state value in the state dictionary.
+        /// </summary>
         public string Key { get; }
+
+        /// <summary>
+        /// Indicates whether the property value should only be read from the underlying state store or also be written to.
+        /// </summary>
+        public bool ReadOnly { get; }
 
         /// <summary>
         /// The key will match the property name this attribute is applied to.
@@ -14,9 +22,14 @@ namespace LittleBlazors.Components.Wizards
         {
         }
 
-        public StateAttribute(string key)
+        public StateAttribute(string key) : this(key, false)
+        {
+        }
+
+        public StateAttribute(string key, bool readOnly)
         {
             this.Key = key;
+            this.ReadOnly = readOnly;
         }
     }
 }
